250ef453037cb306876f5f93f615ba0dd301dcaf
Chen <ccheng.go@foxmail.com>
2018-11-20 16:24:21 +0800

    修改内容位置

--- src/android/java/com/sunmi/cordova/SunmiPlugin.java
+++ src/android/java/com/sunmi/cordova/SunmiPlugin.java
@@ -3,6 +3,7 @@ package com.sunmi.cordova;
 import com.alibaba.fastjson.JSONArray;
 import com.alibaba.fastjson.JSONObject;
 import com.sunmi.cordova.utils.AidlUtil;
+import com.sunmi.cordova.utils.ESCUtil;
 import com.sunmi.cordova.utils.Json;
 
 import org.apache.cordova.CallbackContext;
@@ -17,7 +18,6 @@ public class SunmiPlugin extends CordovaPlugin {
     public void initialize(CordovaInterface cordova, CordovaWebView webView) {
         super.initialize(cordova, webView);
         AidlUtil.getInstance().connectPrinterService(webView.getContext());
-        AidlUtil.getInstance().initPrinter();
     }
 
     @Override
@@ -48,12 +48,18 @@ public class SunmiPlugin extends CordovaPlugin {
             }
 
             if ("printText".equals(type)) {
+                AidlUtil.getInstance().sendRawData(ESCUtil.alignLeft());
+                AidlUtil.getInstance().initPrinter();
                 AidlUtil.getInstance().printText(text, size, isBold, isUnderLine);
             } else if ("print3Line".equals(type)) {
                 AidlUtil.getInstance().print3Line();
             } else if ("printBarCode".equals(type)) {
-                AidlUtil.getInstance().printBarCode(text, 8, height, width, 1);
+                AidlUtil.getInstance().sendRawData(ESCUtil.alignCenter());
+                AidlUtil.getInstance().initPrinter();
+                AidlUtil.getInstance().printBarCode(text, 8, height, width, 2);
             } else if ("printQr".equals(type)) {
+                AidlUtil.getInstance().sendRawData(ESCUtil.alignCenter());
+                AidlUtil.getInstance().initPrinter();
                 AidlUtil.getInstance().printQr(text, size, 3);
             } else {
                 callbackContext.error("type is empty");
